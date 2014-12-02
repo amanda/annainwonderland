16d06986c6dd47a56af8087332b5237c40fe1d19
amandapickering <pickering.amanda@gmail.com>
2014-12-02 13:44:02 -0500

    cast input works!

--- annainwonderland.py
+++ annainwonderland.py
@@ -3,14 +3,11 @@ import nltk
 from nltk import pos_tag, ne_chunk, word_tokenize
 from nltk.tokenize import SpaceTokenizer
 from collections import Counter
-import operator
 
 def people_extractor(text):
 	'''returns a list of people in a text,
 	people are determined by nltk's named entity
 	chunk (ne_chunk) function'''
-	#tokenizer = SpaceTokenizer()
-	#tags = pos_tag(tokenizer.tokenize(text))
 	tags = pos_tag(word_tokenize(text))
 	chunked = ne_chunk(tags) #Tree
 	people = [' '.join(map(lambda x: x[0], entity.leaves())) for entity in chunked 
@@ -18,28 +15,24 @@ def people_extractor(text):
 	return people
 
 def list_frequent(people_list):
-	'''reorders list by most frequent values'''
-	people_count = dict(Counter(people_list))
-	sorted_count = sorted(people_count.items(), key=operator.itemgetter(1))[::-1]
-	sorted_people = [name for name, value in sorted_count]
-	return sorted_people
+	'''reorders list by most frequent entries'''
+	people_count = Counter(people_list)
+	sorted_count = sorted(people_count.keys(), key=people_count.get, reverse=True)
+	return sorted_count
 
 def make_cast(roles, players):
 	'''maps people from one list to another
 	keys are roles (to be replaced), values are players'''
-	#length = equalize_list_length(roles, players)
-	#cast = {roles[i]: players[i] for i in range(length)}
 	zipped = zip(roles, players) #list of tuples
 	cast = dict(zipped)
 	return cast
 
 def file_tokens(text_file):
-	'''for getting tokens from a text file'''
+	'''for getting tokens from a text file
+	using nltk's SpaceTokenizer'''
 	with open(text_file, 'r') as f:
 		tokenizer = SpaceTokenizer()
 		return tokenizer.tokenize(f.read())
-		# tokens = word_tokenize(f.read())
-		return tokens
 
 def insert_people(cast_dict, dest_tokens):
 	'''cast dict keys are roles, value is who plays that role.
@@ -51,7 +44,6 @@ if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('people', type=file, help='text file to get names from')
 	parser.add_argument('plot', type=file, help='text to put names in')
-	#parser.add_argument('-l' '--list', action='store_true')
 	args = parser.parse_args()
 	people_text = ' '.join((args.people).readlines()).decode('utf-8')
 	plot_text = ' '.join((args.plot).readlines()).decode('utf-8')
--- cast_annainwonderland.py
+++ cast_annainwonderland.py
@@ -3,14 +3,11 @@ import nltk
 from nltk import pos_tag, ne_chunk, word_tokenize
 from nltk.tokenize import SpaceTokenizer
 from collections import Counter
-import operator
 
 def people_extractor(text):
 	'''returns a list of people in a text,
 	people are determined by nltk's named entity
 	chunk (ne_chunk) function'''
-	#tokenizer = SpaceTokenizer()
-	#tags = pos_tag(tokenizer.tokenize(text))
 	tags = pos_tag(word_tokenize(text))
 	chunked = ne_chunk(tags) #Tree
 	people = [' '.join(map(lambda x: x[0], entity.leaves())) for entity in chunked 
@@ -18,10 +15,10 @@ def people_extractor(text):
 	return people
 
 def list_frequent(people_list):
-	'''reorders list by most frequent values'''
+	'''reorders list by most frequent entries'''
 	people_count = Counter(people_list)
-	sorted_count = sorted(people_count.keys(), key=people_count.get, reversed=True)
-	return sorted_people
+	sorted_count = sorted(people_count.keys(), key=people_count.get, reverse=True)
+	return sorted_count
 
 def make_cast(roles, players):
 	'''maps people from one list to another
@@ -31,7 +28,8 @@ def make_cast(roles, players):
 	return cast
 
 def file_tokens(text_file):
-	'''for getting tokens from a text file'''
+	'''for getting tokens from a text file
+	using nltk's SpaceTokenizer'''
 	with open(text_file, 'r') as f:
 		tokenizer = SpaceTokenizer()
 		return tokenizer.tokenize(f.read())
@@ -43,17 +41,24 @@ def insert_people(cast_dict, dest_tokens):
 	return replaced
 
 def get_cast_from_user():
+	'''for easy user input'''
 	players = raw_input('enter the names of people you would like to insert into the text, separated by commas: ')
-	return [x.strip() for x in players]
+	return [x.strip() for x in players.split(',')]
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument('plot', type=file, help='text to put names in')
+	parser.add_argument('-l', '--list', type=file, nargs='?', help='optional input list')
 	args = parser.parse_args()
 	plot_text = ' '.join((args.plot).readlines()).decode('utf-8')
-	people_list = get_cast_from_user()
+	if not args.list:
+		people_list = get_cast_from_user()
+	else:
+		people_list = args.list.read().split(',') #hacky for list of hacker schoolers
+		print people_list
 	plot_list = people_extractor(plot_text)
 	most_people = list_frequent(people_list)
+	print most_people
 	most_plot = list_frequent(plot_list)
 	cast = make_cast(most_plot, most_people)
 	tokenizer = SpaceTokenizer()
