97cb2c77511d3f35a97150ce29d68173a8b1e4d4
amandapickering <pickering.amanda@gmail.com>
2014-12-30 15:05:01 -0500

    trying (and failing) at more cleanup

--- some_cleanup.py
+++ some_cleanup.py
@@ -1,18 +1,24 @@
 import re
+from nltk import word_tokenize
 
-HS_PATH = '/Users/amp/Dropbox/amanda/projects/python/nanogenmo/annainwonderland/hs_pride_swapped_clean.txt'
+HS_PATH = '/Users/amp/Dropbox/amanda/projects/python/nanogenmo/annainwonderland/hs_pride_swapped.txt'
 
 def cleanup_punc(text):
 	new = re.sub(r'\s+([,.;!])', r'\1', text)
-	return new
+	no_ticks = re.sub(r'\s+([`])', r'\1', text)
+	return no_ticks
 
 def split_lines(tokens):
 	for i in range(len(tokens)):
 		if i % 10 == 0:
 			tokens.insert(i, "\n")
-	return tokens
+	return ' '.join(tokens)
+
+with open(HS_PATH, 'r') as f:
+	messy = f.read()
 
 if __name__ == '__main__':
-	with open(HS_PATH) as f:
-		cleanup_punc(f.read())
-		split_lines(f.read())
+	cleaner = cleanup_punc(messy)
+	splitty = split_lines(word_tokenize(messy))
+	with open('hs_pride_swapped_clean.txt', 'w') as f:
+		f.write(splitty)
