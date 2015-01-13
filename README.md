bc1cc8c22ad7945d063ed43ea97c16b84cf3e745
amandapickering <pickering.amanda@gmail.com>
2015-01-13 17:59:09 -0500

    major refactor, now writes to file with line breaks

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ annainwonderland
 
 anna karenina in wonderland: takes people entities from one text and swaps them with the people in another.
 
-sample output: https://gist.github.com/amandapickering/19d3675a7aed32cd40c9
+## usage
+
+python annainwonderland.py sourcetextfile [list of people to insert]
--- annainwonderland.py
+++ annainwonderland.py
@@ -1,15 +1,10 @@
-'''put new people in classic works of literature.
-warning: this will take a really long time for books 
-in its current state! but it "works" (poorly).
-todo: fix output punct and newlines...'''
-import argparse
-import nltk
+#!/bin/env python
+
+import argparse, os, os.path, pickle, codecs, nltk, re
 from nltk import pos_tag, ne_chunk, word_tokenize
 from collections import Counter
-import os.path
-import os
 
-def people_extractor(tokens, pos_tags):
+def people_extractor(pos_tags):
 	'''returns a list of people in a text,
 	people are determined by nltk's named entity
 	chunk (ne_chunk) function'''
@@ -27,52 +22,58 @@ def list_frequent(people_list):
 def make_cast(roles, players):
 	'''maps people from one list to another
 	keys are roles (to be replaced), values are players'''
-	zipped = zip(roles, players)
-	cast = dict(zipped)
+	cast = dict(zip(roles, players))
 	return cast
-
-def insert_people(cast_dict, dest_tokens): #dest_tokens must match ne_chunk
-	'''cast dict keys are roles, value is who plays that role.
-	dest_tokens must have names of roles/keys.'''
-	replaced = [cast_dict.get(x, x) for x in dest_tokens]
-	return replaced
-
-def tokenize_chunks(chunked):
-	treewords = []
-	for chunk in chunked:
-		if isinstance(chunk, nltk.tree.Tree):
-			treewords.append(' '.join([x[0] for x in chunk.leaves()]))
-		else:
-			treewords.append(chunk[0])
-	return treewords #maintains people entities as tokens
 
 def get_cast_from_user():
 	'''for easy user input'''
 	players = raw_input('enter the names of people you would like to insert into the text, separated by commas: ')
 	return [x.strip() for x in players.split(',')]
 
+
+def people_replacer(textfile, cast_dict):
+	'''textfile, list -> new textfile 
+	with new people inserted'''
+	p = re.compile('|'.join(re.escape(x) for x in cast_dict))
+	filename, ext = os.path.splitext(textfile)
+	swapped_file = filename + '-swapped' + ext
+	with codecs.open(textfile, 'r', encoding='utf-8') as infile:
+		with codecs.open(swapped_file, 'w') as outfile:
+			for line in infile:
+				outfile.write(p.sub(repl(cast_dict), line))
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser()
-	parser.add_argument('plot', type=file, help='text to put names in')
+	parser.add_argument('plot', help='text to put names in')
 	parser.add_argument('-l', '--list', type=file, nargs='?', help='optional input list')
 	args = parser.parse_args()
-	plot_text = args.plot.read()
 	if not args.list:
 		people_list = get_cast_from_user()
 	else:
 		people_list = args.list.read().split(',') #hacky for list of hacker schoolers
-	tokens = word_tokenize(plot_text)
-	tags = pos_tag(tokens)
-	plot_list = people_extractor(tokens, tags)
+	
+	print 'inserting your characters...'
+
+	with open(args.plot) as f:
+		tags = pos_tag(word_tokenize(f.read()))
+	plot_list = people_extractor(tags)
+	print 'plot list: {}'.format(plot_list)
 	most_people = list_frequent(people_list)
 	most_plot = list_frequent(plot_list)
 	cast = make_cast(most_plot, most_people)
-	print cast
-	try:
-		swapped = insert_people(cast, tokenize_chunks(ne_chunk(tags)))
-	except UnicodeEncodeError:
-		print "working on it!"
-	#print ' '.join(swapped)
-	output_file = '{0}_swapped.txt'.format(args.plot.name[:-4])
-	with open(output_file, 'w') as f: #careful, will overwrite
-		f.write(' '.join(swapped))
+	print 'cast: {}'.format(cast)
+
+	def repl(match):
+		return cast[match.group(0)]
+
+	p = re.compile('|'.join(re.escape(x) for x in cast))
+	
+	filename, ext = os.path.splitext(args.plot)
+	swapped_file = filename + '-swapped' + ext
+	
+	with codecs.open(args.plot, 'r', encoding='utf-8') as infile:
+		with codecs.open(swapped_file, 'w') as outfile:
+			for line in infile:
+				outfile.write(p.sub(repl, line))
+	#people_replacer(args.plot, cast)
+	print 'done'
